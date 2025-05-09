025cd21c9f4cc95ea6ec992958b7a53f64a12ebf
Arthur Rimbun <arthur.rimbun@gmail.com>
2025-05-09 21:02:04 +1000

    Solve p16 better

--- solutions/16.py
+++ solutions/16.py
@@ -2,10 +2,7 @@ from typing import List
 
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        # numbers array must be sorted
-        sorted_nums = sorted(nums)
-
-        # iunfinit
+        sorted_nums = sorted(nums)          # numbers array must be sorted
         best_delta = float('inf')
         result = []
 
@@ -14,33 +11,26 @@ class Solution:
             b = a+1                         # 2nd pointer = 1st pointer + 1
             c = len(sorted_nums)-1          # 3rd pointer starts from the last element
 
-            # this stores the previous delta we found during iteration
-            delta_prev = float('inf')
             while b < c:
                 i = sorted_nums[a]
                 j = sorted_nums[b]
                 k = sorted_nums[c]
                 ijk = i+j+k
-                delta = abs(target-ijk)
 
+                # if delta is lower than before, save this as best result
+                delta = abs(target-ijk)
                 if delta < best_delta:
-                    # if delta is closer to target than best delta, then set it
-                    # as the best delta and store [i,j,k] as the new best result
+                    result = [i,j,k]
                     best_delta = delta
-                    result = [i, j, k]
-                    delta_prev = delta
-                elif delta <= delta_prev:
-                    # if delta is getting smaller then continue iterating;
-                    # we may be able to find an even better delta
+
+                if ijk >= target:
+                    # if sum is larger than target then move 3rd pointer left
+                    # (to smaller number) to try and reduce delta
                     c -= 1
-                    delta_prev = delta
-                elif delta > delta_prev:
-                    # if delta is getting larger then it is pointless to keep
-                    # iterating 3rd pointer, instead it is time to advance 2nd
-                    # pointer
+                else:
+                    # if sum is smaller than target then move 2nd pointer right
+                    # (to larger number) to try and reduce delta
                     b += 1
-                    c = len(sorted_nums)-1
-                    delta_prev = float('inf')
 
         return sum(result)
 
@@ -57,7 +47,7 @@ nums3 = [-84,92,26,19,-7,9,42,-51,8,30,-100,-13,-38]
 target3 = 78
 
 sol = Solution()
-print(sol.threeSumClosest(nums, target))
-print(sol.threeSumClosest(nums1, target1))
-print(sol.threeSumClosest(nums2, target2))
+print(sol.threeSumClosest(nums, target))    # 2
+print(sol.threeSumClosest(nums1, target1))  # 0
+print(sol.threeSumClosest(nums2, target2))  # 3
 print(sol.threeSumClosest(nums3, target3))  # 77
